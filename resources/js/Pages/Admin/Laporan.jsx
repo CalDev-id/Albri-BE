b9178cc43df020292671f551e2b9cb31de14f904
Heical Chandra <haical31@gmail.com>
2024-10-27 20:21:10 +0700

    update sidebar

---
+++ resources/js/Pages/Admin/Laporan.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Laporan = () => {
+    return (
+        <DefaultLayout>
+            <div>
+                <h1>Laporan</h1>
+            </div>
+        </DefaultLayout>
+    );
+}
+
+export default Laporan;
---
+++ resources/js/Pages/Admin/Mitra.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Mitra = () => {
+    return (
+        <DefaultLayout>
+            <div>
+                <h1>Mitra</h1>
+            </div>
+        </DefaultLayout>
+    );
+}
+
+export default Mitra;
