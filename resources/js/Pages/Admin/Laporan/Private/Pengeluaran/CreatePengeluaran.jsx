0f989fd112a942191635b2abf63aff10c9586b91
Heical Chandra <haical31@gmail.com>
2024-11-16 15:48:15 +0700

    update

--- resources/js/Pages/Admin/Laporan/Private/Pengeluaran/CreatePengeluaran.jsx
+++ resources/js/Pages/Admin/Laporan/Private/Pengeluaran/CreatePengeluaran.jsx
@@ -39,7 +39,7 @@ const CreatePengeluaran = () => {
                 <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
                     <div className="border-b border-stroke px-6.5 py-4 dark:border-strokedark">
                         <h3 className="font-medium text-black dark:text-white">
-                            Laporan Pemasukan Cabang
+                            Laporan Pengeluaran Private
                         </h3>
                     </div>
                     <form onSubmit={handlesubmit}>
