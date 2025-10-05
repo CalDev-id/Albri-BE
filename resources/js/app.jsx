b551e25c7c2c376e72c9ce46fb43368ffb4c86dc
Eugene702 <eugenefeilianputrarangga@gmail.com>
2025-10-05 11:28:21 +0700

    Title

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,7 +5,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = "Rumah Belajar Alrbri";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
