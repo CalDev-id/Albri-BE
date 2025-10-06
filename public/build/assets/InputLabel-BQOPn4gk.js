ed17e30853fa096656d3cf63b014e8c1c5b1c2ac
Eugene702 <eugenefeilianputrarangga@gmail.com>
2025-10-06 08:55:47 +0700

    Feat: Re-Build

---
+++ public/build/assets/InputLabel-BQOPn4gk.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-BqKozNyH.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
